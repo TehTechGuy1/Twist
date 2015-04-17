8b06cec70f787ab0534de09ce88e7b65fbd61bf7
TehTechGuy1 <alanoreilly2010@hotmail.co.uk>
2015-04-17 17:34:52 +0100

    Test auto-update

--- Twisted Dream/src/com/alan/gfg/utils/Updater.java
+++ Twisted Dream/src/com/alan/gfg/utils/Updater.java
@@ -4,7 +4,7 @@ import com.alan.gfg.utils.files.TextFile;
 
 public class Updater {
 	private static String currentVersion, newVersion;
-	public static int update =0;
+	public static int update = 0;
 	
 	public static void checkForUpdate(boolean isAuto){
 		currentVersion = TextFile.readFile("./version.txt");
